c25f563d5806eff5e357fe7018d472215c6fe360
drillyme <lakhanthakurboss@gmail.com>
2022-11-12 12:07:01 +0530

    my commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer" >
-            <div className="footer-container" >
-                <div>
-                    <h3 style={{ fontWeight: "normal" }} >Copyright &copy; {new Date().getFullYear()}</h3>
-                </div>
-                <div >
-                    <h3 style={{ fontWeight: "normal" }}>
-                        Made by 
-                        <span>
-                            <a href="https://github.com/anshuman9999" 
-                            style={{ color: "#2691d9" }} 
-                            target="_blank"
-                            rel="noreferrer"
-                            > Anshuman Pandey</a>
-                        </span>
-                    </h3>
-                </div>
-
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div>
+          <h3 style={{ fontWeight: "normal" }}>Copyright &copy; {new Date().getFullYear()}</h3>
+        </div>
+        <div>
+          <h3 style={{ fontWeight: "normal" }}>
+            Made by
+            <span>
+              <a href="https://github.com/drillyme" style={{ color: "#2691d9" }} target="_blank" rel="noreferrer">
+                {" "}
+                Lakhan Pundhir
+              </a>
+            </span>
+          </h3>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
